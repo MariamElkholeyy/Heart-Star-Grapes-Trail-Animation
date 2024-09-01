b9075d31cf7035989542b6076fd1b590742b8e63
Mariam Elkholey <mariamelkholey.grand@gmail.com>
2024-09-01 15:24:02 +0300

    added more shapes

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-// const bodyEl = document.querySelector("body");
-
-// bodyEl.addEventListener("mousemove", function (e) {
-//     const xPos = e.offsetX;
-//     const yPos = e.offsetY;
-//     console.log(xPos, yPos);
-
-//     const spanEl = document.createElement("span");
-//     spanEl.style.left = xPos + "px";
-//     spanEl.style.top = yPos + "px";
-
-//     bodyEl.appendChild(spanEl);
-// });
+const shapes = $('.shapes');
 const bodyEl = $('body');
-bodyEl.on('mousemove' , function (e) {
+let isShapeClicked = false;
+
+shapes.on('click', function(e) {
+  var shape = $(e.target); // Get the target element of the click event
+  var shapeClass = shape.attr('class');
+  console.log(shapeClass);
+  shapes.remove(); // Remove the shape after clicking
+
+  // Set the background image of the original span element
+  switch (shapeClass) {
+    case 'heart':
+      $('span').addClass('heart-style');
+      break;
+    case 'star':
+      $('span').addClass('star-style');
+      break;
+    case 'grape':
+      $('span').addClass('grape-style');
+      break;
+  }
+
+  isShapeClicked = true;
+
+  // Stop the trail after 3 seconds
+  setTimeout(() => {
+    isShapeClicked = false;
+  }, 3000);
+});
+
+bodyEl.on('mousemove', function(e) {
+  if (isShapeClicked) {
     const x = e.clientX;
     const y = e.clientY;
-    console.log(x, y);
-    console.log(e);
 
     const spanEl = $("<span></span>");
-    spanEl.css('left' , x + 'px');
-    spanEl.css('top' , y + 'px');
-    spanEl.css('position' , "absolute");
+    spanEl.css('left', x + 'px');
+    spanEl.css('top', y + 'px');
+    spanEl.css('position', "absolute");
     const size = Math.random() * 100;
-    spanEl.css('width' , size + 'px');
-    spanEl.css('height' , size + 'px');
+    spanEl.css('width', size + 'px');
+    spanEl.css('height', size + 'px');
+    spanEl.css('background', $('span').css('background')); // Use the same background image as the original span element
 
     bodyEl.append(spanEl);
     setTimeout(() => {
-        spanEl.remove();
-      }, 3000);
-
-})
+      spanEl.remove();
+    }, 3000);
+  }
+});
